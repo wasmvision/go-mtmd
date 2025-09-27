fbc8fc115db4b375a31a07b74334bae78c3552cb
deadprogram <ron@hybridgroup.com>
2025-09-28 00:17:36 +0200

    refactor: simplify and cleanup bitmap.
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- pkg/mtmd/bitmap.go
+++ pkg/mtmd/bitmap.go
@@ -12,7 +12,6 @@ import (
 // MTMD_API uint32_t              mtmd_bitmap_get_nx     (const mtmd_bitmap * bitmap);
 // MTMD_API uint32_t              mtmd_bitmap_get_ny     (const mtmd_bitmap * bitmap);
 // MTMD_API const unsigned char * mtmd_bitmap_get_data   (const mtmd_bitmap * bitmap);
-// MTMD_API size_t                mtmd_bitmap_get_n_bytes(const mtmd_bitmap * bitmap);
 // // bitmap ID is optional, but useful for KV cache tracking
 // // these getters/setters are dedicated functions, so you can for example calculate the hash of the image based on mtmd_bitmap_get_data()
 // MTMD_API const char * mtmd_bitmap_get_id(const mtmd_bitmap * bitmap);
@@ -23,20 +22,19 @@ type Bitmap uintptr
 
 var (
 	// MTMD_API mtmd_bitmap *         mtmd_bitmap_init           (uint32_t nx, uint32_t ny, const unsigned char * data);
-	BitmapInit     func(nx uint32, ny uint32, data uintptr) Bitmap
 	bitmapInitFunc ffi.Fun
 
 	// MTMD_API void                  mtmd_bitmap_free       (mtmd_bitmap * bitmap);
-	BitmapFree     func(bitmap Bitmap)
 	bitmapFreeFunc ffi.Fun
 
 	// MTMD_API size_t                mtmd_bitmap_get_n_bytes(const mtmd_bitmap * bitmap);
-	BitmapGetNBytes     func(bitmap Bitmap) uint32
 	bitmapGetNBytesFunc ffi.Fun
 
 	// MTMD_API mtmd_bitmap * mtmd_helper_bitmap_init_from_file(mtmd_context * ctx, const char * fname);
-	BitmapInitFromFile     func(ctx Context, fname string) Bitmap
 	bitmapInitFromFileFunc ffi.Fun
+
+	// MTMD_API mtmd_bitmap * mtmd_helper_bitmap_init_from_buf(mtmd_context * ctx, const unsigned char * buf, size_t len);
+	bitmapInitFromBufFunc ffi.Fun
 )
 
 func loadBitmapFuncs(lib ffi.Lib) {
@@ -44,38 +42,58 @@ func loadBitmapFuncs(lib ffi.Lib) {
 	if bitmapInitFunc, err = lib.Prep("mtmd_bitmap_init", &ffi.TypePointer, &ffi.TypeSint32, &ffi.TypeSint32, &ffi.TypePointer); err != nil {
 		panic(err)
 	}
-	BitmapInit = func(nx uint32, ny uint32, data uintptr) Bitmap {
-		var bitmap Bitmap
-		bitmapInitFunc.Call(unsafe.Pointer(&bitmap), &nx, &ny, unsafe.Pointer(&data))
-
-		return bitmap
-	}
 
 	if bitmapFreeFunc, err = lib.Prep("mtmd_bitmap_free", &ffi.TypeVoid, &ffi.TypePointer); err != nil {
 		panic(err)
-	}
-	BitmapFree = func(bitmap Bitmap) {
-		bitmapFreeFunc.Call(nil, unsafe.Pointer(&bitmap))
 	}
 
 	if bitmapGetNBytesFunc, err = lib.Prep("mtmd_bitmap_get_n_bytes", &ffi.TypeUint32, &ffi.TypePointer); err != nil {
 		panic(err)
 	}
-	BitmapGetNBytes = func(bitmap Bitmap) uint32 {
-		var result ffi.Arg
-		bitmapGetNBytesFunc.Call(unsafe.Pointer(&result), unsafe.Pointer(&bitmap))
-
-		return uint32(result)
-	}
 
 	if bitmapInitFromFileFunc, err = lib.Prep("mtmd_helper_bitmap_init_from_file", &ffi.TypePointer, &ffi.TypePointer, &ffi.TypePointer); err != nil {
 		panic(err)
 	}
-	BitmapInitFromFile = func(ctx Context, fname string) Bitmap {
-		var bitmap Bitmap
-		file := &[]byte(fname + "\x00")[0]
-		bitmapInitFromFileFunc.Call(unsafe.Pointer(&bitmap), unsafe.Pointer(&ctx), unsafe.Pointer(&file))
 
-		return bitmap
+	if bitmapInitFromBufFunc, err = lib.Prep("mtmd_helper_bitmap_init_from_buf", &ffi.TypePointer, &ffi.TypePointer, &ffi.TypePointer); err != nil {
+		panic(err)
 	}
+}
+
+// BitmapInit initializes a Bitmap.
+func BitmapInit(nx uint32, ny uint32, data uintptr) Bitmap {
+	var bitmap Bitmap
+	bitmapInitFunc.Call(unsafe.Pointer(&bitmap), &nx, &ny, unsafe.Pointer(&data))
+
+	return bitmap
+}
+
+// BitmapFree frees a previous initialized Bitmap.
+func BitmapFree(bitmap Bitmap) {
+	bitmapFreeFunc.Call(nil, unsafe.Pointer(&bitmap))
+}
+
+// BitmapGetNBytes returns the number of bytes in the Bitmap.
+func BitmapGetNBytes(bitmap Bitmap) uint32 {
+	var result ffi.Arg
+	bitmapGetNBytesFunc.Call(unsafe.Pointer(&result), unsafe.Pointer(&bitmap))
+
+	return uint32(result)
+}
+
+// BitmapInitFromFile initializes a Bitmap from a file.
+func BitmapInitFromFile(ctx Context, fname string) Bitmap {
+	var bitmap Bitmap
+	file := &[]byte(fname + "\x00")[0]
+	bitmapInitFromFileFunc.Call(unsafe.Pointer(&bitmap), unsafe.Pointer(&ctx), unsafe.Pointer(&file))
+
+	return bitmap
+}
+
+// BitmapInitFromBuf initializes a Bitmap from a buffer of bytes.
+func BitmapInitFromBuf(ctx Context, buf *byte, len uint64) Bitmap {
+	var bitmap Bitmap
+	bitmapInitFromBufFunc.Call(unsafe.Pointer(&bitmap), unsafe.Pointer(&ctx), unsafe.Pointer(&buf), &len)
+
+	return bitmap
 }
