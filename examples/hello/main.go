d0e5403d90572cb7b121412ade816a5e5d7c363f
deadprogram <ron@hybridgroup.com>
2025-09-29 15:08:03 +0200

    examples: simplify hello, world example.
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- examples/hello/main.go
+++ examples/hello/main.go
@@ -9,9 +9,9 @@ import (
 
 var (
 	modelFile            = "./models/tinyllama-1.1b-chat-v1.0.Q2_K.gguf"
-	prompt               = "Are you ready to go?"
+	prompt               = "Are you ready to rock?"
 	libPath              = "./lib"
-	responseLength int32 = 32
+	responseLength int32 = 64
 )
 
 func main() {
@@ -19,50 +19,26 @@ func main() {
 	llama.Load(lib)
 
 	llama.BackendInit()
-	defer llama.BackendFree()
-
 	llama.LogSet(llama.LogSilent(), uintptr(0))
 
 	model := llama.ModelLoadFromFile(modelFile, llama.ModelDefaultParams())
-	defer llama.ModelFree(model)
-
 	vocab := llama.ModelGetVocab(model)
 
 	sampler := llama.SamplerChainInit(llama.SamplerChainDefaultParams())
 	llama.SamplerChainAdd(sampler, llama.SamplerInitGreedy())
 
-	// call once to get the size
+	// call once to get the size of the tokens from the prompt
 	count := llama.Tokenize(vocab, prompt, nil, true, false)
 
 	// now get the actual tokens
 	tokens := make([]llama.Token, count)
 	llama.Tokenize(vocab, prompt, tokens, true, false)
 
-	ctxParams := llama.ContextDefaultParams()
-	ctxParams.NCtx = uint32(count + responseLength)
-	ctxParams.NBatch = uint32(count)
-
-	lctx := llama.InitFromModel(model, ctxParams)
-	defer llama.Free(lctx)
-
+	lctx := llama.InitFromModel(model, llama.ContextDefaultParams())
 	batch := llama.BatchGetOne(tokens)
-
-	if llama.ModelHasEncoder(model) {
-		llama.Encode(lctx, batch)
-
-		start := llama.ModelDecoderStartToken(model)
-		if start == llama.TOKEN_NULL {
-			start = llama.VocabBOS(vocab)
-		}
-
-		batch = llama.BatchGetOne([]llama.Token{start})
-	}
-
 	for pos := int32(0); pos+batch.NTokens < count+responseLength; pos += batch.NTokens {
 		llama.Decode(lctx, batch)
-
 		token := llama.SamplerSample(sampler, lctx, -1)
-		llama.SamplerAccept(sampler, token)
 
 		if llama.VocabIsEOG(vocab, token) {
 			fmt.Println()
