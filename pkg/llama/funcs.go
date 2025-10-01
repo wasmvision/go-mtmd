be9e6d8e6ddbf5d1b088ac4c019614f43f3b759a
deadprogram <ron@hybridgroup.com>
2025-10-01 09:28:45 +0200

    feature: add additional memory functions and move to context
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- pkg/llama/funcs.go
+++ pkg/llama/funcs.go
@@ -16,17 +16,6 @@ var (
 
 	// GGML_API void ggml_backend_load_all(void);
 	ggmlBackendLoadAllFromPath ffi.Fun
-
-	// LLAMA_API void llama_memory_clear(
-	// 	llama_memory_t mem,
-	// 				bool data);
-	memoryClearFunc ffi.Fun
-
-	// LLAMA_API           llama_memory_t   llama_get_memory  (const struct llama_context * ctx);
-	getMemoryFunc ffi.Fun
-
-	// LLAMA_API void llama_synchronize(struct llama_context * ctx);
-	synchronizeFunc ffi.Fun
 )
 
 func loadFuncs(lib ffi.Lib) error {
@@ -44,18 +33,6 @@ func loadFuncs(lib ffi.Lib) error {
 	}
 
 	if ggmlBackendLoadAllFromPath, err = lib.Prep("ggml_backend_load_all_from_path", &ffi.TypeVoid, &ffi.TypePointer); err != nil {
-		return err
-	}
-
-	if memoryClearFunc, err = lib.Prep("llama_memory_clear", &ffi.TypeVoid, &ffi.TypePointer, &ffi.TypeUint8); err != nil {
-		return err
-	}
-
-	if getMemoryFunc, err = lib.Prep("llama_get_memory", &ffi.TypePointer, &ffi.TypePointer); err != nil {
-		return err
-	}
-
-	if synchronizeFunc, err = lib.Prep("llama_synchronize", &ffi.TypeVoid, &ffi.TypePointer); err != nil {
 		return err
 	}
 
@@ -81,19 +58,4 @@ func GGMLBackendLoadAll() {
 func GGMLBackendLoadAllFromPath(path string) {
 	p := &[]byte(path + "\x00")[0]
 	ggmlBackendLoadAllFromPath.Call(nil, unsafe.Pointer(&p))
-}
-
-func MemoryClear(mem Memory, data bool) {
-	memoryClearFunc.Call(nil, unsafe.Pointer(&mem), unsafe.Pointer(&data))
-}
-
-func GetMemory(ctx Context) Memory {
-	var mem Memory
-	getMemoryFunc.Call(unsafe.Pointer(&mem), unsafe.Pointer(&ctx))
-
-	return mem
-}
-
-func Synchronize(ctx Context) {
-	synchronizeFunc.Call(nil, unsafe.Pointer(&ctx))
 }
